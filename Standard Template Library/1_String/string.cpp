e2d4bc538639f9508e647f3a8b8a594116941ae8
Shuvo Sarker <Shuvo01797451@gmail.com>
2025-02-05 17:56:49 +0600

    string compare

---
+++ Standard Template Library/1_String/string.cpp
@@ -0,0 +1,11 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+    string s1 = "Hello";
+    string s2 = "World";
+
+    // Compare string
+    cout << (s1 == s2); // return 0/1
+    return 0;
+}
