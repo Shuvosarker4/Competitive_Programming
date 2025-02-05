059698be69702cb54c9aa5ae82a08a2cbe213489
Shuvo Sarker <Shuvo01797451@gmail.com>
2025-02-05 16:26:11 +0600

    New

---
+++ 1_Basic_Cpp/1_Basic_Structure/struc.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int a;    // variable declared
+    cin >> a; // take input in variable a
+
+    cout << a; // show output of variable a
+    return 0;
+}
---
+++ 1_Basic_Cpp/2_TypeCast/typ.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    // Get Assci value of any char
+    char c;
+    cin >> c;
+
+    int asci = c;
+    cout << asci << endl;
+    return 0;
+}
